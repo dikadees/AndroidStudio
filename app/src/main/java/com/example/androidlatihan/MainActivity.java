280741b4e5a8511fc6540379dd1acb4bbc24628b
dika_dees <dikaduck@gmail.com>
2020-07-06 18:04:25 +0700

    menambahkan onClick dan activity_welcome.xml dan welcome.java

--- app/src/main/java/com/example/androidlatihan/MainActivity.java
+++ app/src/main/java/com/example/androidlatihan/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.androidlatihan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnlogin;
     RelativeLayout rellay1, rellay2;
 
     Handler handler = new Handler();
@@ -24,10 +27,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnlogin = findViewById(R.id.btnlogin);
 
         rellay1 = (RelativeLayout) findViewById(R.id.rellay1);
         rellay2 = (RelativeLayout) findViewById(R.id.rellay2);
 
         handler.postDelayed(runnable, 2000); // timeout for splash
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // action
+                Intent intent = new Intent(MainActivity.this, welcome.class);
+                startActivity(intent);
+            }
+        });
     }
 }
