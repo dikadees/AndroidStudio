cd1df7e788576260d1636ece66030c547dba25db
dika_dees <dikaduck@gmail.com>
2020-07-10 14:38:37 +0700

    menambahkan validation error sign in dan signup

--- app/src/main/java/com/example/androidlatihan/MainActivity.java
+++ app/src/main/java/com/example/androidlatihan/MainActivity.java
@@ -7,12 +7,15 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnlogin;
-    private Button btnsignup;
+    private Button btnlogin, btnsignup;
+    private EditText username, password;
+
     RelativeLayout rellay1, rellay2;
 
     Handler handler = new Handler();
@@ -31,20 +34,34 @@ public class MainActivity extends AppCompatActivity {
 
         btnlogin = findViewById(R.id.btnlogin);
         btnsignup = findViewById(R.id.btnsignup);
+        username = findViewById(R.id.username_login);
+        password = findViewById(R.id.password_login);
 
         rellay1 = (RelativeLayout) findViewById(R.id.rellay1);
         rellay2 = (RelativeLayout) findViewById(R.id.rellay2);
 
         handler.postDelayed(runnable, 2000); // timeout for splash
 
+
         btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                // action
-                Intent intent = new Intent(MainActivity.this, welcome.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                if(username.getText().toString().length()==0 && password.getText().toString().length()==0){
+                    username.setError("Username harus diisi");
+                    password.setError("Password harus diisi");
+                } else if (username.getText().toString().length()==0){
+                    username.setError("Password harus diisi");
+                } else if (password.getText().toString().length()==0){
+                    password.setError("Password harus diisi");
+                } else {
+                    Toast.makeText(getApplicationContext(), "Registrasi" +
+                            "berhasil", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, welcome.class);
+                    startActivity(intent);
+                }
             }
         });
+
         btnsignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
 
     }
 }
--- app/src/main/java/com/example/androidlatihan/signup.java
+++ app/src/main/java/com/example/androidlatihan/signup.java
@@ -6,15 +6,67 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 public class signup extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
+    private EditText username, fullname, email, password;
+    private Button daftar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        username = (EditText) findViewById(R.id.username);
+        fullname = (EditText) findViewById(R.id.fullname);
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        daftar = (Button) findViewById(R.id.btn_signup);
+
+        daftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int getUsername = username.getText().toString().length();
+                int getFullname = fullname.getText().toString().length();
+                int getEmail = email.getText().toString().length();
+                int getPassword = password.getText().toString().length();
+
+
+                if (getUsername == 0 && getFullname==0 && getEmail == 0 && getPassword == 0){
+                    username.setError("Username harus diisi");
+                    fullname.setError("Nama tidak boleh kosong");
+                    email.setError("Email masih kosong");
+                    password.setError("Password harus diisi");
+                    Toast.makeText(getApplicationContext(), "Registrasi" +
+                            "gagal", Toast.LENGTH_SHORT).show();
+                } else if(getUsername == 0){
+                    username.setError("Username harus diisi");
+                    Toast.makeText(getApplicationContext(), "Masukkan" +
+                            "username", Toast.LENGTH_SHORT).show();
+                } else if(getFullname == 0){
+                    fullname.setError("Full name tidak boleh kosong");
+                    Toast.makeText(getApplicationContext(), "Masukkan" +
+                            "full name", Toast.LENGTH_SHORT).show();
+                } else if (getEmail == 0){
+                    email.setError("Email masih kosong");
+                    Toast.makeText(getApplicationContext(), "Masukkan" +
+                            "email", Toast.LENGTH_SHORT).show();
+                } else if (getPassword == 0){
+                    password.setError("Password harus diisi");
+                    Toast.makeText(getApplicationContext(), "Masukkan" +
+                            "password", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Registrasi" +
+                            "berhasil", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         Spinner colorSpinner = findViewById(R.id.spinnerRole);
         ArrayAdapter adapter = ArrayAdapter.createFromResource(
@@ -36,4 +88,7 @@ public class signup extends AppCompatActivity implements AdapterView.OnItemSelec
     public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
+
+
+
 }
